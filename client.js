59cfedf1fb9a0282105f48718dd6c36bf0435a7f
Angel Sinn <angelkysinn@gmail.com>
2021-02-10 20:53:39 +0000

    Updated snake initials

---
+++ client.js
@@ -0,0 +1,30 @@
+const { on } = require("events");
+const net = require("net");
+
+/*
+Establishes connection with the game server
+*/
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: "135.23.222.131",
+    port: 50542,
+  });
+  // Interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("data", (data) => {
+    console.log(`Message: ${data}`);
+  });
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server!");
+    conn.write("Name: SKY");
+  });
+
+  return conn;
+};
+
+module.exports = {
+  connect,
+};
--- play.js
+++ play.js
@@ -1,24 +1,4 @@
-const { on } = require("events");
-const net = require("net");
-
-/*
-Establishes connection with the game server
-*/
-
-const connect = function () {
-  const conn = net.createConnection({
-    host: "135.23.222.131",
-    port: 50542,
-  });
-  // Interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("data", function (data) {
-    console.log(`Message: ${data}`);
-  });
-
-  return conn;
-};
+const { connect } = require("./client");
 
 console.log("Connecting...");
 connect();
